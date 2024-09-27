72ab9486be87a9e66b6359e12f9df817aa29bd02
owais ali <owaisali72359@gmail.com>
2024-09-27 18:53:20 +0500

    signUp add backend Functionality

--- SignUp Form/backend/server.js
+++ SignUp Form/backend/server.js
@@ -1,20 +1,22 @@
 import express from "express"
 import cors from "cors"
+import bodyParser from "body-parser"
 const app = express()
 const port = 3000
 
 
 app.use(cors())
+app.use(bodyParser.text())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worldneea!')
 })
 app.post('/', (req, res) => {
-    console.log(req);
+  console.log(req.body);
 
-    
-    res.send('Hello World!')
-  })
+
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- SignUp Form/src/App.jsx
+++ SignUp Form/src/App.jsx
@@ -13,29 +13,29 @@ function App() {
   } = useForm()
 
 
-// const delay = (d)=> {
-//   return new Promise((reslove , reject)=>{
-//     setTimeout(()=>{
+  const delay = (d) => {
+    return new Promise((reslove, reject) => {
+      setTimeout(() => {
 
-//       reslove()
+        reslove()
 
-//     }, d * 1000)
-    
-//   })
-  
-// }
+      }, d * 1000)
 
+    })
+
+  }
   const onSubmit = async (data) => {
-    
-    // await delay(2)
 
-    let a = await fetch("http://localhost:3000/", {method:"POST"})
+    await delay(2)
+
+    let a = await fetch("http://localhost:3000/", { method: "POST", body: JSON.stringify(data) })
     let res = await a.text()
 
-    // if(data.firstName !== "Owais" || data.firstName !== "Sarosh" || data.firstName !== "Ebad"){
-    //   setError("forms" , {message:"Invalid Credintial"})
-    // }
-    console.log(data , res)}
+    if (data.firstName !== "Owais" && data.firstName !== "Sarosh" && data.firstName !== "Ebad") {
+      setError("forms", { message: "Invalid Credintial" })
+    }
+    console.log(data, res)
+  }
 
 
   return (
@@ -45,31 +45,31 @@ function App() {
       <br />
       {isSubmitting && <div className='text-2xl text-red-600'>Loading...</div>}
       <form onSubmit={handleSubmit(onSubmit)} action="">
-        
-        <input className='w-1/4 h-10 text-md p-1 border-purple-950  rounded-md' {...register("firstName", { required: { value:true, message: "This is reuired field" }, minLength: { value: 3, message: "minLength is 3" }, maxLength: { value: 10, message: "maxLength is 10" } })} type="text " placeholder='Enter First Name'  />
+
+        <input className='w-1/4 h-10 text-md p-1 border-purple-950  rounded-md' {...register("firstName", { required: { value: true, message: "This is reuired field" }, minLength: { value: 3, message: "minLength is 3" }, maxLength: { value: 10, message: "maxLength is 10" } })} type="text " placeholder='Enter First Name' />
         <br />
         {errors.firstName && <span className='text-red-600'>{errors.firstName.message}</span>}
         <br />
-        <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("lastName", { required: { value: true, message: "This is reuired field" }, minLength: { value: 3, message: "minLength is 3" }, maxLength: { value: 10, message: "maxLength is 10" } })} type="text " placeholder='Enter Last Name'  />
+        <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("lastName", { required: { value: true, message: "This is reuired field" }, minLength: { value: 3, message: "minLength is 3" }, maxLength: { value: 10, message: "maxLength is 10" } })} type="text " placeholder='Enter Last Name' />
         <br />
         {errors.lastName && <span className='text-red-600'>{errors.lastName.message}</span>}
         <br />
-        <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("email", { required: { value: true, message: "This is reuired field" } })} type="email " placeholder='Enter Email'   />
-       <br />
+        <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("email", { required: { value: true, message: "This is reuired field" } })} type="email " placeholder='Enter Email' />
+        <br />
         {errors.email && <span className='text-red-600'>{errors.email.message}</span>}
         <br />
-        
-        <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("phone", { required: { value: true, message: "This is reuired field" }, minLength: { value: 11, message: "minLength is 11" }, maxLength: { value: 15, message: "maxLength is 15" } })}  type="number " placeholder='Enter Phone number' />
-       <br />
+
+        <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("phone", { required: { value: true, message: "This is reuired field" }, minLength: { value: 11, message: "minLength is 11" }, maxLength: { value: 15, message: "maxLength is 15" } })} type="number " placeholder='Enter Phone number' />
+        <br />
         {errors.phone && <span className='text-red-600'>{errors.phone.message}</span>}
         <br />
-       
-       <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("password", { required: { value: true, message: "This is reuired field" }, minLength: { value: 7, message: "minLength is 7" }, maxLength: { value: 15, message: "maxLength is 15" } })} type="password " placeholder='Enter Password'    />
-       <br />
+
+        <input className='w-1/4 h-10 text-md p-1 rounded-md' {...register("password", { required: { value: true, message: "This is reuired field" }, minLength: { value: 7, message: "minLength is 7" }, maxLength: { value: 15, message: "maxLength is 15" } })} type="password " placeholder='Enter Password' />
+        <br />
         {errors.password && <span className='text-red-600'>{errors.password.message}</span>}
         <br />
         <input className='bg-slate-900 text-white w-28 h-9 rounded-md' disabled={isSubmitting} type="submit" value="submit" />
-       <br />
+        <br />
         {errors.forms && <span className='text-red-600'>{errors.forms.message}</span>}
       </form>
     </>
